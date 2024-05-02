6b2c0efd81b2365253400abc25142209d552ac99
Luis Eduardo <luiseduardf.a@gmail.com>
2024-05-02 20:30:08 -0300

    create header

--- my-list/src/components/Header/Header.tsx
+++ my-list/src/components/Header/Header.tsx
@@ -1,16 +1,23 @@
-import styles from "./styles.module.css";
+import styles from "./styles.module.scss";
+import { StartCards } from "../StatsCard/StatsCard";
 
 export const Header: React.FC = () => {
     return (
         <header className={styles.header}>
-            <div>
-                <h1>MyTodo</h1>
+            <div className={styles.conteiner}>
+                <div>
+                    <h1>MyTodo</h1>
 
-                <span>Bem Vindo!</span>
-            </div>
-            <div>
+                    <span>Bem Vindo!</span>
+                </div>
+                <div>
+                    <StartCards title="Total de Tarefas" value={5}/>
+                    <StartCards title="Tarefas Pendentes" value={4} />
+                    <StartCards title="Tarefas concluídas" value={1}/>
+                </div>
 
             </div>
+            
         </header>
     );
 }
